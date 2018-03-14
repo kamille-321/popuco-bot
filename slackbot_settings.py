5eee4c973abfcd72573b65e8e37bd227abc0d393
kamille-321 <yuji.kmjm@gmail.com>
2018-03-14 18:06:42 +0900

    a

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 # botアカウントのトークンを指定
-API_TOKEN = SLACKBOT_API_TOKEN
+#API_TOKEN = SLACKBOT_API_TOKEN
+API_TOKEN = os.environ.get('POPUCO_SLACK_API_KEY', '')
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "意味わかんねーよ死ね バーーーーーーーーーーーカ!!!"
 
