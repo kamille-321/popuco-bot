257454ee8f6541a27313b9f84d32d45141d4cf33
kamille-321 <yuji.kmjm@gmail.com>
2018-03-17 01:10:10 +0900

    ref

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -3,7 +3,6 @@ import os
 
 # botアカウントのトークンを指定
 API_TOKEN = os.environ.get("CSLACK_API_KEY")
-#API_TOKEN = os.getenv("CSLACK_API_KEY")
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "意味わかんねーよ死ね バーーーーーーーーーーーカ!!!"
